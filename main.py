71567c9fd641619c54c6fe7cd4cdefe7a84fed40
Dhruv Chawla <dchawla228@gmail.com>
2022-07-28 10:33:36 +0530

    added ngrok with flask

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from slack import WebClient
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 # Load .env file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = WebClient(token=os.environ['SLACK_TOKEN'])
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 
-client.chat_postMessage(channel='#test', text='Hello world!')
+slack_web_client = WebClient(token=os.environ['SLACK_TOKEN'])
+
+slack_web_client.chat_postMessage(channel='#test', text='Hello world!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
